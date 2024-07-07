f6317b79f43cf57bb12b89735aa83d0a521a6787
Leandro Silva da Silva <leandro.tecnicoinfo@gmail.com>
2024-07-07 12:13:15 -0300

    integracao Postgres

--- core/settings.py
+++ core/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'stokar',
+        'USER': 'postgres',
+        'PASSWORD': '328679',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
