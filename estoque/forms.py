bd2d85e67ab8d0854ac87d02a965d8ee90a28e58
Paulo Peter <eduardopeter91@gmail.com>
2024-07-13 19:35:30 -0300

    Ajustando - Forms e Alimento_create

--- estoque/forms.py
+++ estoque/forms.py
@@ -7,10 +7,12 @@ class AlimentoForm(forms.ModelForm):
         model = Alimento
         fields = ['nome', 'quantidade', 'validade', 'peso']
         widgets = {
-            'validade': forms.DateInput(attrs={'type': 'date'}),
+            'nome': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nome do alimento'}),
+            'quantidade': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Quantidade'}),
+            'validade': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'peso': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Peso em Kg'}),
         }
 
-    #Define formatação padrão de que cada palavra contida no nome deve iniciar com maiúscula; auxilia na integridade
     def clean_nome(self):
         nome = self.cleaned_data.get('nome', '')
         return nome.title()
